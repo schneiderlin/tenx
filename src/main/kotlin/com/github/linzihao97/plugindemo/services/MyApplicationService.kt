ce8931dfa4bebb20465a9edcca510c4235c50ecc
GitHub Action <action@github.com>
2022-07-07 07:31:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/linzihao97/plugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/linzihao97/plugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.linzihao97.plugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linzihao97.plugindemo.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/linzihao97/plugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/linzihao97/plugindemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.linzihao97.plugindemo.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linzihao97.plugindemo.MyBundle
 
 class MyProjectService(project: Project) {
 
