f460aa8b52047fd7029dc2b6cae2b92550328de9
linzihao <zihao.lin@shopcider.com>
2022-10-25 22:39:53 +0800

    my json editor

--- src/main/java/com/example/linzihao97/plugindemo/TextAreaDialog.java
+++ src/main/java/com/example/linzihao97/plugindemo/TextAreaDialog.java
@@ -1,13 +1,23 @@
 package com.example.linzihao97.plugindemo;
 
+import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.ide.highlighter.HighlighterFactory;
 import com.intellij.json.JsonFileType;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.util.Condition;
+import com.intellij.psi.*;
 import com.intellij.ui.EditorTextField;
 import com.intellij.ui.components.JBPanel;
+import com.intellij.util.LocalTimeCounter;
+import com.intellij.util.TextFieldCompletionProvider;
+import com.intellij.util.textCompletion.TextCompletionUtil;
+import com.intellij.util.textCompletion.TextFieldWithCompletion;
 import com.intellij.util.ui.JBDimension;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -72,9 +82,20 @@ public class TextAreaDialog extends DialogWrapper {
     }
 
     private class JSONEditor extends EditorTextField {
+        private final FileType fileType = JsonFileType.INSTANCE;
+
         public JSONEditor(String text, Project project) {
             super(text, project, JsonFileType.INSTANCE);
-            super.setDocument(super.createDocument());
+            super.setDocument(createDocument(text));
+        }
+
+        protected Document createDocument(String initText) {
+            final PsiFileFactory factory = PsiFileFactory.getInstance(project);
+            final long stamp = LocalTimeCounter.currentTime();
+            final PsiFile psiFile = factory.createFileFromText("Dummy." + fileType.getDefaultExtension(), fileType, initText, stamp, true, false);
+
+            //TextCompletionUtil.installProvider(psiFile, new MyTextFieldCompletionProvider(), true);
+            return PsiDocumentManager.getInstance(project).getDocument(psiFile);
         }
 
         @Override
@@ -86,6 +107,14 @@ public class TextAreaDialog extends DialogWrapper {
             ex.setOneLineMode(false);
 
             return ex;
+        }
+    }
+
+    private static class MyTextFieldCompletionProvider extends TextFieldCompletionProvider implements DumbAware {
+        @Override
+        protected void addCompletionVariants(@NotNull String text, int offset, @NotNull String prefix, @NotNull CompletionResultSet result) {
+            result.addElement(LookupElementBuilder.create("123456"));
+            result.addElement(LookupElementBuilder.create("234567"));
         }
     }
 }
